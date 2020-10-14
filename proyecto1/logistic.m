67fe01c5f6e55f626f53e7fbe6d58db528fbea0c
Pablo Alvarado-Moya <palvaradomoya@gmail.com>
2020-10-14 10:31:38 -0600

    Baseline code for the first project

---
+++ proyecto1/logistic.m
@@ -0,0 +1,13 @@
+#!/usr/bin/octave-cli
+
+## Copyright (C) 2020 Pablo Alvarado
+##
+## Este archivo forma parte del material del Proyecto 1 del curso:
+## EL5852 Introducción al Reconocimiento de Patrones
+## Escuela de Ingeniería Electrónica
+## Tecnológico de Costa Rica
+
+## Función logística 1/1+exp(-x)
+function l=logistic(x)
+  l=1./(1+exp(-x));
+endfunction
